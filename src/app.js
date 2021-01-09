0dfc6b98d217c545698c3e6342da23f2d4a02366
manish <manish@DEVOPS.INTERNAL>
2021-01-09 16:53:52 +0530

    Removed Dummy Data

--- src/app.js
+++ src/app.js
@@ -12,22 +12,6 @@ import getVisibleExpenses from './selectors/expenses';
 
 const store = configureStore();
 
-store.dispatch(sortByDate())
-
-store.dispatch(addExpense({ description: 'Water bill', amount:4500}));
-store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
-
-// store.dispatch(setTextFilter('water'));
-
-// setTimeout(() => {
-//   store.dispatch(setTextFilter('bill'));
-// }, 3000)
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const JSX = ()=>{
     return (
         <div>
